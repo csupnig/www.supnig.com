148fdc9ca54c132c6ff857a4d45e73062afcbb07
Christopher Supnig <christopher@supnig.com>
2014-12-12 13:55:21 +0100

    facebook login return url

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
 		facebook: {
 			clientID: "194935747224798",
 			clientSecret: "356f4c3d963cce5585c420e82e195671",
-			callbackURL: "http://localhost:8081/auth/facebook/callback"
+			callbackURL: "http://www.supnig.com/auth/facebook/callback"
 		},
 
 		google: {
