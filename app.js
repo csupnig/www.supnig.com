e753e47cf4810843547cba47495ed1d9a260f7b6
Christopher Supnig <christopher@supnig.com>
2017-03-28 13:13:46 +0200

    deploy

--- app.js
+++ app.js
@@ -473,5 +473,24 @@ app.get('/sitemap', function (req, res) {
     });
 });
 
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
 app.listen(port);
 console.log('Finished setting up server on port ' + port);
