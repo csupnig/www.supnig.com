c28cee58b9c2ead8a9f104f2d7297822abb7068f
Christopher Supnig <christopher@supnig.com>
2019-01-28 10:17:17 +0100

    work on website

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pwd
-aws s3 sync dist/. s3://www.supnig.com
+aws s3 sync dist/. s3://www.supnig.com --profile=supnig
 echo "Invalidating cloudfrond distribution to get fresh cache"
-aws cloudfront create-invalidation --distribution-id=EDGHFTN6MY6WO --paths /\* --profile=default
+aws cloudfront create-invalidation --distribution-id=EDGHFTN6MY6WO --paths /\* --profile=supnig
